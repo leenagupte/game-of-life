86c9769c34e7fe20047c6c985927e7757237e39c
Leena Gupte <leena.gupte@gmail.com>
2015-01-08 18:34:38 +0000

    Made it print

--- unit_tests/test_game_of_life.py
+++ unit_tests/test_game_of_life.py
@@ -1,7 +1,9 @@
 import unittest
+
 from hamcrest import equal_to, assert_that
 from mock import patch
-import copy
+
+from game_of_life import get_cell_state, set_cell_state, get_next_day_universe, get_number_of_alive_neighbours
 
 
 class TestGameOfLife(unittest.TestCase):
@@ -67,7 +69,7 @@ class TestGameOfLife(unittest.TestCase):
                     equal_to(expected_universe))
 
 
-    @patch('test_game_of_life.get_cell_state')
+    @patch('game_of_life.get_cell_state')
     def test_get_state_of_neighbour(self, get_cell_state_mock):
         universe = [
             [0, 0, 0, 0, 0],
@@ -182,63 +184,3 @@ class TestGameOfLife(unittest.TestCase):
 
         assert_that(get_next_day_universe(yesterdays_universe),
                     equal_to(expected_universe))
-
-
-def get_cell_state(universe, position_x, position_y):
-
-    state = universe[position_y][position_x]
-
-    return state
-
-
-def set_cell_state(universe, position_x, position_y, state):
-    universe[position_y][position_x] = state
-
-def get_next_day_universe(yesterdays_universe):
-
-    # get the number of alive neighbours for each cell
-    # if the number of alive neighbours is less than 2
-    # change state of cell to dead
-
-    universe = copy.deepcopy(yesterdays_universe)
-
-    y = 0;
-
-    for row in universe:
-        x = 0;
-        for cell in row:
-            number_of_alive_neigbours = \
-                get_number_of_alive_neighbours(yesterdays_universe, x, y)
-
-            if number_of_alive_neigbours < 2:
-                set_cell_state(universe, x, y, 0)
-
-            if number_of_alive_neigbours > 3:
-                set_cell_state(universe, x, y, 0)
-
-            if number_of_alive_neigbours == 3:
-                set_cell_state(universe, x, y, 1)
-
-            x += 1
-        y += 1
-
-    return universe
-
-
-def get_number_of_alive_neighbours(universe, position_x, position_y):
-
-    count = 0
-
-    for x in range(position_x-1, position_x+2):
-        for y in range(position_y-1, position_y+2):
-            if not (x == position_x and y == position_y):
-                try:
-                    if get_cell_state(universe, x, y):
-                        count += 1
-                except:
-                    pass
-    return count
-
-
-if __name__ == "__main__":
-    unittest.main()
